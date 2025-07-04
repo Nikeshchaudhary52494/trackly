7d83a11a865f372099a84d9b2787539693223471
nikesh chaudhary <nikeshchaudhary52494@gmail.com>
2025-07-04 20:40:32 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center bg-black h-full">
+      <p className=" text-white font-bold text-5xl italic">TRACKLY</p>
+    </div>
+  );
+}
